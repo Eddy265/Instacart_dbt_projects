552d5132d323defcbeddd11f791af555433ccc52
Edwin Uzoefuna <euzoefuna@rocketmail.com>
2023-07-30 12:30:54 +0200

    Updated the fact orders table to incremental model

--- models/intermediate/fct_orders.sql
+++ models/intermediate/fct_orders.sql
@@ -1,33 +1,40 @@
---fact table for orders
 {{
-    config (materialized = 'table')
+    config(
+        materialized='incremental',
+        unique_key='order_id'
+    )
 }}
 
 WITH product AS (
     SELECT product_id, unit_price, unit_cost
-    FROM {{ref ("stg_products")}} 
+    FROM {{ ref("stg_products") }} 
 ),
 
-orders as (
-    select order_id,
-            customer_id,
-            product_id,
-            quantity,
-            order_date
-     from {{ref ('stg_orders')}}
+orders AS (
+    SELECT order_id,
+           customer_id,
+           product_id,
+           quantity,
+           order_date
+    FROM {{ ref('stg_orders') }}
 ),
 
-FINAL AS (SELECT o.order_id, 
-        o.customer_id,
-        o.product_id,
-        o.order_date,
-        o.quantity,
-        p.unit_price,
-        p.unit_cost,
-        o.quantity * p.unit_price AS order_total_amount,
-        p.unit_price - p.unit_cost AS profit 
-FROM product p 
-JOIN orders o on p.product_id = o.product_id
-ORDER BY profit DESC)
+FINAL AS (
+    SELECT o.order_id, 
+           o.customer_id,
+           o.product_id,
+           o.order_date,
+           o.quantity,
+           p.unit_price,
+           p.unit_cost,
+           o.quantity * p.unit_price AS order_total_amount,
+           p.unit_price - p.unit_cost AS profit 
+    FROM product p 
+    JOIN orders o ON p.product_id = o.product_id
+    {% if is_incremental() %}
+    WHERE order_id >= (SELECT max(order_id) FROM {{ ref('stg_orders') }})
+    {% endif %}
+    ORDER BY order_id DESC
+)
 
-select * from FINAL
+SELECT * FROM FINAL
